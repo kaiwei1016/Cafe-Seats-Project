e5bcae8021e0ffe1976052ca86906385dede7e9f
Minfangtsai <mftsai2004@gmail.com>
2025-06-06 14:30:37 +0800

    Update IM_db_server.py
    配合ssh啟動

--- IM_db_server.py
+++ IM_db_server.py
@@ -173,3 +173,6 @@ def get_bg_setting(floor_id: str):
     doc["id"] = str(doc["_id"])
     return doc
 
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8002)
